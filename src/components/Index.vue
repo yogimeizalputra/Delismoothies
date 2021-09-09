a507f252f3f72b7c1288e13eb0e6412446125145
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-09-09 23:06:03 +0700

    Deleting Firestore data

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -26,8 +26,11 @@ export default {
   },
   methods: {
     deleteSmoothie(id) {
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id != id
+      db.collection('smoothies').doc(id).delete()
+      .then(() => {
+          this.smoothies = this.smoothies.filter(smoothie => {
+            return smoothie.id != id
+          })
       })
     }
   },
