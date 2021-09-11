6a1e8ae0bc821b62d54a041bc9bc56bd044ae0b2
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-09-11 07:40:50 +0700

    Adding Ingredients

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -1,49 +1,62 @@
 <template>
-    <div class="add-smoothie container">
-        <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
-        <form @submit.prevent="AddSmoothie">
-            <div class="field title">
-                <label for="title">Smoothie Title</label>
-                <input type="text" name="title" v-model="title">
-            </div>
-            <div class="field add-ingredient">
-                <label for="add-ingredient">Add an ingredient: </label>
-                <input type="text" name="add-ingredient">
-            </div>
-            <div class="field center-align">
-                <button class="btn pink">Add Smoothie</button>
-            </div>
-        </form>
-    </div>
+  <div class="add-smoothie container z-depth-1">
+    <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+    <form @submit.prevent="addSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie title:</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient (press tab to add):</label>
+        <input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another">
+      </div>
+      <div class="field center-align">
+        <p v-if="feedback" class="red-text">{{ feedback }}</p>
+        <button class="btn pink">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'AddSmoothie',
-    data () {
-        return{
-            title: null
-        }
+  name: 'AddSmoothie',
+  data(){
+    return{
+      title: null,
+      ingredients: [],
+      another: null,
+      feedback: null
+    }
+  },
+  methods: {
+    addSmoothie(){
+      console.log(this.title, this.ingredients)
     },
-    methods: {
-        AddSmoothie() {
-            
-        }
-    },
+    addIng(){
+      if(this.another){
+        this.ingredients.push(this.another)
+        this.another = null
+        this.feedback = null
+      } else {
+        this.feedback = 'You must enter a value to an an ingredient'
+      }
+    }
+  }
 }
 </script>
 
 <style>
-    .add-smoothie {
-        margin-top: 60px;
-        padding: 20px;
-        max-width: 500px;
-    }
-    .add-smoothie h2 {
-        font-size: 2em;
-        margin: 20px auto;
-    }
-    .add-smoothie .field {
-        margin: 20px auto;
-    }
+.add-smoothie{
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.add-smoothie h2{
+  font-size: 2em;
+  margin: 20px auto;
+}
+.add-smoothie .field{
+  margin: 20px auto;
+}
 </style>
