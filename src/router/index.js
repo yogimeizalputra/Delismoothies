7a60f0ee0a4c69e030710bc9bc8c7821c3dc467d
YMP-BLACRUSH <67401418+YMP-BLACRUSH@users.noreply.github.com>
2021-09-20 19:57:46 +0700

    Edit Smoothie Route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import AddSmoothie from '../components/AddSmoothie.vue'
+import EditSmoothie from '../components/EditSmoothie.vue'
 
 
 const routes = [
@@ -21,6 +22,11 @@ const routes = [
     path: '/add-smoothie',
     name: 'AddSmoothie',
     component: AddSmoothie
+  },
+  {
+    path: '/edit-smoothie/:smoothie_slug',
+    name: 'EditSmoothie',
+    component: EditSmoothie
   }
 ]
 
